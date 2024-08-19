7d0063227655cde31d3d1a08821520fcf9fb82e5
Argox2 <gaelrojotrujillo@gmail.com>
2024-08-19 17:35:05 -0600

    rot13

--- rot3.py
+++ rot3.py
@@ -1,2 +1,18 @@
 def rot13(s):
-    pass
+    result = []
+    for v in s: 
+        c = ord(v)
+
+        if 'a' <= v <= 'z':
+            c = ord('a') + (c - ord('a') + 13) % 26
+        elif 'A' <= v <= 'Z':
+            c = ord('A') + (c - ord('A') + 13) % 26
+        
+        result.append(chr(c))
+    return ''.join(result)
+
+s = input("rot13: ")
+
+res = rot13(s)
+
+print(res)
